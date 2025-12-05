b9118917f87af50c12e2a1ff85ee76551779cb4b
Alshaan <alshaan077@gmail.com>
2025-12-05 22:12:35 +0530

    Use API_URL env instead of localhost

--- src/ChatWindow.jsx
+++ src/ChatWindow.jsx
@@ -106,6 +106,7 @@ import Chat from "./Chat.jsx";
 import { MyContext } from "./MyContext.jsx";
 import { useContext, useState, useEffect } from "react";
 import { ScaleLoader } from "react-spinners";
+const API_URL = import.meta.env.VITE_API_URL;
 
 function ChatWindow() {
     const { prompt, setPrompt, reply, setReply, currThreadId, setPrevChats, setNewChat } = useContext(MyContext);
@@ -177,7 +178,7 @@ function ChatWindow() {
         };
 
         try {
-            const response = await fetch("http://localhost:8080/api/chat", options);
+            const response = await fetch(`${API_URL}/api/chat`, options);
             const res = await response.json();
             console.log(res);
             setReply(res.reply);
